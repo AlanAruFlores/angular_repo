c8f2cfe9b25e132776ffd0bafd0dbd59a71b277a
AlanAruFlores <alanaruquipa242@gmail.com>
2024-10-01 22:23:04 -0300

    primer proyecto node

---
+++ mi-aplicacion-nodejs/global_variables.js
@@ -0,0 +1,5 @@
+/*En nodejs existen varias globales a usarse */
+
+console.log(__dirname); // Devuelve la carpeta del arcchivo actual (ruta del directorio)
+console.log(__filename); // devuelve el fichero mas su carpeta  (ruta completa)
+
